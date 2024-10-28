770a4cfbe75638259bce9ec4c07fa77b9ea55b6c
shlokstha <shlok.dibtech@gmail.com>
2024-10-28 19:28:08 +0545

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const Home = async () => {
 
   return (
     <main className="max-w-5xl mx-auto my-5">
-      <h1 className="text-3xl font-bold mb-4">Blogs page</h1>
+      <h1 className="text-3xl font-bold mb-4">Blogs pagess</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {posts.map((post, index) => (
           <BlogCard post={post} key={index} />
