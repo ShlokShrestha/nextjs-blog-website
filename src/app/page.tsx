6caa29ae6f8a3fd32102fe086841283372946dd7
shlokstha <shlok.dibtech@gmail.com>
2024-12-29 17:19:22 +0545

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ const Home = async () => {
       author: true,
     },
   });
-
   return (
     <main className="max-w-5xl mx-auto my-5">
       <h1 className="text-3xl font-bold mb-4">Blogs pagess</h1>
